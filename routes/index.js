5a9b0556c0dbd046f8ea0cb1916a240928793101
anish25 <anishgandhi25@gmail.com>
2019-06-14 23:43:41 -0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+ 
+var app = express();
+
+app.use(express.static('public'))
+app.use(express.static('views'))
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/Services', function(req, res, next) {
+  res.render('services', { title: 'Services' });
+});
+
+router.get('/Projects', function(req, res, next) {
+  res.render('project', { title: 'Projects' });
+});
+
+router.get('/About', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
+
+router.get('/Contact_Us', function(req, res, next) {
+  res.render('contact_us', { title: 'Contact_Us' });
+});
+
+module.exports = router;
